10915382ddb99b9188175b0ae518895fe0ed6767
Jonny Lea <lea.jonny@gmail.com>
2021-06-04 14:37:55 +0100

    Initial commit

---
+++ octopus/client/src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+test('should be able to increase and decrease product quantity', async () => {
+  expect(true).toBe(false);
+});
+
+test('should be able to add items to the basket', async () => {
+  expect(true).toBe(false);
+});
+
